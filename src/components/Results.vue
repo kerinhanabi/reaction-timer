cd47d301aac4b3b4adb4af6bc350fe58003c3695
kerinhanabi <kerinhanabi@hotmail.com>
2024-08-26 11:53:31 +0800

    second commit

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
   <div>Reaction time: {{ score }} ms</div>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
     props: ['score'],
+    data() {
+      return {
+        rank: null
+      }
+    },
+    mounted() {
+      if (this.scrore < 250) {
+        this.rank = 'Thunder speed!'
+      } else if (this.rank < 400) {
+        this.rank = 'Rapid reflexes!'
+      } else {
+        this.rank = 'snail pace...'
+      }
+    }
 }
 </script>
 
 <style>
-
+.rank {
+  font-size: 24px;
+  color: #0faf87;
+  font-weight: bold;
+  }
 </style>
